87e35e6a60569616549cfede4020f33e06348170
Gayatri-2017 <gayatrig1997@gmail.com>
2018-10-24 22:41:12 +0530

    Improved log formatting

--- DDOS_Mitigation.sh
+++ DDOS_Mitigation.sh
@@ -15,12 +15,15 @@ echo "Packet information stored in output.txt"
 
 file=output.txt
 flag=0
-minimumsize=3000000
+minimumsize=3000000 # 3 MB
 #tcpdump -G 5 -W 1 -w output.txt -i en0 'port 8080'
 
 while true ; do
 	#timeout 5 tcpdump -i en0 -w output.txt
-	tcpdump -G 5 -W 1 -w output.txt -i en0 udp
+	#tcpdump -G 5 -W 1 -w output.txt -i en0 -nni udp
+	#tcpdump -G 5 -W 1 -nni en0 udp -w output.txt
+	#tcpdump -nni en0 udp -G 5 -W 1 >output.txt
+	gtimeout 5 tcpdump -nni en0 udp > output.txt #runs for 5 sec
     actualsize=$(wc -c <"$file")
     echo "File size is " $actualsize
     if [ $actualsize -ge $minimumsize ]; then
@@ -28,29 +31,40 @@ while true ; do
         flag=1
     fi
     if [ $flag -eq 1 ] ; then
-    	echo "DDOS attack suspected. Mitigation begins"
-    	sudo ifconfig en0 down
+    	echo "DDOS attack suspected. Mitigation begins."
+    	#sudo ifconfig en0 down
+    	#echo "Network is down now."
     	#give options to open tcpdump file or restart network
-        PS3='What will you like to do next?'
-options=("View tcpdump file" "Restore your network" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "View tcpdump file")
-            echo "you chose to View tcpdump file"
-            cat $file
-            ;;
-        "Restore your network")
-            echo "you chose Restore your network"
-            sudo ifconfig en0 up
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
+        PS3="What will you like to do next?"$'\n'
+
+		options=("View tcpdump file" "Restore your network" "Continue Detection")
+		select opt in "${options[@]}"
+		do
+    		case $opt in
+        		"View tcpdump file")
+            		echo "you chose to View tcpdump file"
+            		open $file
+            		
+            	;;
+       			 "Restore your network")
+            		echo "you chose Restore your network"
+            		#sudo ifconfig en0 down
+            		#sudo ifconfig en0 up
+            		#networksetup -setairportpower en0 off
+            		#networksetup -setairportpower en0 on
+            		
+            	;;
+        		"Continue Detection")
+            		break
+            	;;
+        		*) 
+					echo "invalid option $REPLY";;
+    		esac
+		done
+		read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 	fi
+	#echo "Do you want to Quit the program"
+	
 done
 
 #echo "Size of $FILENAME = $FILESIZE bytes."
